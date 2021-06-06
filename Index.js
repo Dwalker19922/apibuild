453669e2815f5ff02f4afde2423199421b94afa1
Daniel Walker <dw765608@gmail.com>
2021-06-06 18:08:24 -0400

    wip

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const server = express()
-const PORT= process.env.PORT||4000
+const PORT= process.env.PORT
 server.use(express.json())
 server.use(cors())
 server.get('/api/hello', (req, res) => {
